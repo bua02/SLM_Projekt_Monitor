c11c98d42789dd7f65352aa14ec9d66586056037
Julian Rektenwald <julian.rektenwald@Jul-MBP.local>
2022-12-03 16:05:10 +0100

    Returned the controller

--- src/main/java/com/fhtw/slm_projekt_monitor/MonitorController.java
+++ src/main/java/com/fhtw/slm_projekt_monitor/MonitorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.fhtw.slm_projekt_monitor.MessageUtil.EVERYTHING_WORKS_AS_EXPECTED_MESSAGE;
 
-@RestController
+@Controller
 public class MonitorController {
     private final Monitor monitor = new Monitor();
 
